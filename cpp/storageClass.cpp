639edd3abe12e0bc0fb586ce4f8ed57f8a5bae4a
kingnan-Guo <373734199@qq.com>
2022-04-21 00:05:42 +0800

    c++ learning

---
+++ cpp/storageClass.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file storageClass.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+// 
+// using namespace std;
+
+static int count = 10;
+
+int main()
+{
+
+    
+
+    std::cout << "变量为 =" << count << std::endl;
+    return 0;
+}
--- cpp/variable_scope.cpp
+++ cpp/variable_scope.cpp
@@ -15,11 +15,31 @@ using namespace std;
 // int i;
 int i = 20, a = 30;
 
+
+
+#define LENGTH 10;
+#define NEWLINE '\n'
 int main()
 {
     int i, a;
     i = 10;
     a = i * 2;
     cout << a << endl;
+    cout << "Hello\tWorld\n\n";
+    string greeting = "hellow kingnan";
+    cout << greeting;
+    cout << NEWLINE;
+    cout << LENGTH;
+    cout << NEWLINE;
+
+    const int width = 3;
+    cout << width;
+    cout << NEWLINE;
+    short int c;
+    short unsigned int j;
+    j = 50000;
+    c = j;
+    cout << c << " " << j; //无符号短整数 50,000 的位模式被解释为有符号短整数 -15,536。
+    
     return 0;
 }
