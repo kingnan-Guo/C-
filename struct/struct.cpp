2ce918681534cca89582857c0b6f54f9648bf676
kingnan-Guo <373734199@qq.com>
2022-04-23 03:40:47 +0800

    结构体

---
+++ struct/struct.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file struct.cpp
+ * @author your name (you@domain.com)
+ * @brief 结构体
+ * @version 0.1
+ * @date 2022-04-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+
+struct Books
+{
+    char title[50];
+    int book_id;
+};
+
+
+
+int main()
+{
+
+
+}
+
