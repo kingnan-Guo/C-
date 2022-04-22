48c3c2d475a1647ccd704353ebf8696578fd0db9
kingnan-Guo <373734199@qq.com>
2022-04-23 03:40:23 +0800

    输入输出

---
+++ outPut/io.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file io.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    char name[10];
+    cin >> name;
+    cout << "name =="<< name<< endl;
+    cerr << "error ==" << name << endl;
+    clog << "clog ==" << name <<  endl;
+
+}
